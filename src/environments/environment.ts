7876e62555ac9d8e6e98215be73ef312b2dc73e6
QUICK APPS STUDIOS <quickappsstudios@QUICKs-iMac.local>
2017-09-09 20:07:33 +0100

    google auth nearly finished

---
+++ src/environments/environment.ts
@@ -0,0 +1,21 @@
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
+
+export const environment = {
+  production: false,
+
+  firebase:{
+
+    apiKey: "AIzaSyAAoLNXyBjbnI5F2vvT82shfgCuhVC7DoY",
+    authDomain: "oshop-63b1a.firebaseapp.com",
+    databaseURL: "https://oshop-63b1a.firebaseio.com",
+    projectId: "oshop-63b1a",
+    storageBucket: "oshop-63b1a.appspot.com",
+    messagingSenderId: "361388020099"
+
+
+  }
+
+};
